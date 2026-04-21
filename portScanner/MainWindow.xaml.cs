9fa70b7e1c3cd472ad30d2e3bd5023f3796772be
FRANCESCO MADONIA <francescomadonia15@gmail.com>
2026-04-21 15:14:29 +0100

    Semplificazione del codice

--- portScanner/MainWindow.xaml.cs
+++ portScanner/MainWindow.xaml.cs
@@ -2,51 +2,60 @@
 using portScanner.Models;
 using System.Diagnostics;
 using System.IO;
-using System.Timers;
 using System.Windows;
 using System.Windows.Threading;
 
-// usare manualResetEvent per stoppare il thread
 namespace portScanner
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-        List<Scansione> Scansioni = new();
-        ManualResetEvent mre = new ManualResetEvent(false);
-        Stopwatch timer = new Stopwatch();
-        DispatcherTimer dispatcherTimer = new DispatcherTimer();
-        private void BtnStartScan_Click(object sender, RoutedEventArgs e)
+
+        List<Scansione> Scansioni;
+        CancellationTokenSource Source;
+        Stopwatch timer;
+        DispatcherTimer dispatcherTimer;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            mre.Set();
+            Source = new CancellationTokenSource();
+            Scansioni = new List<Scansione>();
+            timer = new Stopwatch();
+            dispatcherTimer = new DispatcherTimer();
+        }
+
+        private async void BtnStartScan_Click(object sender, RoutedEventArgs e)
+        {
+            // Reset UI
             LblStatScanned.Text = "0";
             LblStatProgress.Text = "0%";
             LblStatOpen.Text = "0";
             LblStatClosed.Text = "0";
             LblStatFiltered.Text = "0";
             DtgResults.Items.Clear();
+            Scansioni.Clear();
             PbarScanProgress.Value = 0;
             timer.Reset();
             BtnStopScan.IsEnabled = true;
             BtnStartScan.IsEnabled = false;
+
+            Source = new CancellationTokenSource();
+            CancellationToken token = Source.Token;
+
             int PortStart = 0;
             int PortEnd = 0;
             List<int> Porte = new();
+
             try
             {
                 if (TxtCustomPorts.Text != "")
                 {
                     string[] p = TxtCustomPorts.Text.Split(',');
                     foreach (string porta in p)
-                    {
                         Porte.Add(int.Parse(porta.Trim()));
-                    }
                 }
                 else
                 {
@@ -54,69 +63,33 @@ namespace portScanner
                     {
                         switch (CmbPortPreset.SelectedIndex)
                         {
-                            case 1:
-                                PortStart = 1;
-                                PortEnd = 1023;
-                                break;
-                            case 2:
-                                PortStart = 1024;
-                                PortEnd = 49151;
-                                break;
+                            case 1: PortStart = 1; PortEnd = 1023; break;
+                            case 2: PortStart = 1024; PortEnd = 49151; break;
                             case 3:
-                                int[] p = new int[]
+                                Porte = new List<int>
                                 {
-                                    // Essenziali
                                     80, 443, 22, 21, 23, 53, 3389,
-                                    // Mail
                                     25, 110, 143, 587, 465, 993, 995,
-                                    // Database
                                     3306, 5432, 1433, 27017, 6379,
-                                    // Web alternativo
                                     8080, 8443, 8000, 8008, 8081, 8888,
-                                    // Windows/SMB
                                     135, 137, 138, 139, 445,
-                                    // Rete
                                     69, 111, 123, 161, 179, 500, 646,
-                                    // Directory
                                     389, 636, 3268,
-                                    // Remote
                                     512, 513, 514, 873, 1080, 4899, 5631,
-                                    // VPN
-                                    1194, 1723,
-                                    // VNC
-                                    5800, 5900,
-                                    // File sharing
+                                    1194, 1723, 5800, 5900,
                                     548, 2049, 990,
-                                    // Varie
                                     79, 88, 119, 194, 427, 444, 515, 543, 544,
                                     554, 563, 631, 992, 1521, 1900, 2082, 2083,
                                     2121, 2181, 2375, 2376, 3000, 3128, 4444, 5000,
                                     5060, 5985, 5986, 6000, 6379, 6443, 7001, 7070,
                                     8009, 8161, 9000, 9090, 9200, 9300, 9418, 9999,
-                                    10000, 49152,
+                                    10000, 49152
                                 };
-                                Porte = p.ToList();
                                 break;
-                            case 4:
-                                PortStart = 1;
-                                PortEnd = 65535;
-                                break;
-                            case 5:
-                                p = new int[]
-                                {
-                                    80, 443, 8080, 8443
-                                };
-                                Porte = p.ToList();
-                                break;
-                            case 6:
-                                p = new int[]
-                                {
-                                    1433, 1521, 3306, 5432, 27017
-                                };
-                                Porte = p.ToList();
-                                break;
-                            default:
-                                throw new Exception("Nessuna porta selezionata");
+                            case 4: PortStart = 1; PortEnd = 65535; break;
+                            case 5: Porte = new List<int> { 80, 443, 8080, 8443 }; break;
+                            case 6: Porte = new List<int> { 1433, 1521, 3306, 5432, 27017 }; break;
+                            default: throw new Exception("Nessuna porta selezionata");
                         }
                     }
                     else
@@ -127,37 +100,40 @@ namespace portScanner
                 }
 
                 if (Porte.Count == 0)
-                {
                     for (int i = PortStart; i <= PortEnd; i++)
-                    {
                         Porte.Add(i);
-                    }
-                }
-                int aperte = 0;
-                int chiuse = 0;
-                int filtrate = 0;
-                int scansionate = 0;
-                double percentuale = 0;
+
+                // Contatori locali
+                int aperte = 0, chiuse = 0, filtrate = 0, scansionate = 0;
+
+                // Timer UI
                 dispatcherTimer.Interval = TimeSpan.FromMilliseconds(500);
-                dispatcherTimer.Tick += (s, args) => {
+                dispatcherTimer.Tick += (s, args) =>
                     LblElapsedTime.Text = timer.Elapsed.ToString(@"hh\:mm\:ss");
-                };
+
                 LblScanStatus.Text = "Scansione in corso...";
+                timer.Start();
+                dispatcherTimer.Start();
+
                 if (ChkTCP.IsChecked == true)
                 {
                     string target = TxtTarget.Text;
-                    int maxThread = (int)SldThread.Value;
-                    var semaforo = new SemaphoreSlim(maxThread, maxThread);
+                    int maxParallel = Convert.ToInt32(SldThread.Value);
 
-                    var tasks = Porte.Select(async i =>
+                    var semaphore = new SemaphoreSlim(maxParallel);
+
+                    var tasks = Porte.Select(async p =>
                     {
-                        await semaforo.WaitAsync();
-                        mre.WaitOne();
+                        await semaphore.WaitAsync(token);
                         try
                         {
-                            Scansione s = new Scansione(target, i);
-                            await s.ScanTCP();
-                            Dispatcher.Invoke(() =>
+                            token.ThrowIfCancellationRequested();
+
+                            Scansione s = new Scansione(target, p);
+
+                            await s.ScanTCPAsync(token);
+
+                            await Dispatcher.InvokeAsync(() =>
                             {
                                 switch (s.Stato)
                                 {
@@ -173,12 +149,10 @@ namespace portScanner
                                         filtrate++;
                                         LblStatFiltered.Text = filtrate.ToString();
                                         break;
-                                    default:
-                                        throw new Exception("Porta non trovata");
                                 }
                                 scansionate++;
                                 LblStatScanned.Text = scansionate.ToString();
-                                percentuale = 100.0 * scansionate / Porte.Count;
+                                double percentuale = 100.0 * scansionate / Porte.Count;
                                 LblStatProgress.Text = Math.Round(percentuale, 2) + "%";
                                 PbarScanProgress.Value = percentuale;
                                 Scansioni.Add(s);
@@ -187,29 +161,34 @@ namespace portScanner
                         }
                         finally
                         {
-                            semaforo.Release();
+                            semaphore.Release();
                         }
                     });
 
-                    timer.Start();
-                    dispatcherTimer.Start();
-
-                    Task.WhenAll(tasks).ContinueWith(_ =>
-                    {
-                        Dispatcher.Invoke(() =>
-                        {
-                            timer.Stop();
-                            dispatcherTimer.Stop();
-                            BtnStartScan.IsEnabled = true;
-                            BtnStopScan.IsEnabled = false;
-                            LblScanStatus.Text = "Scansione terminata";
-                        });
-                    });
+                    // Aspetta che tutti i task finiscano
+                    await Task.WhenAll(tasks);
                 }
-                mre.Set();
+
+                // Scansione completata
+                timer.Stop();
+                dispatcherTimer.Stop();
+                BtnStartScan.IsEnabled = true;
+                BtnStopScan.IsEnabled = false;
+                LblScanStatus.Text = "Scansione terminata";
+            }
+            catch (OperationCanceledException)
+            {
+                timer.Stop();
+                dispatcherTimer.Stop();
+                BtnStopScan.IsEnabled = false;
+                BtnStartScan.IsEnabled = true;
+                LblScanStatus.Text = "Scansione annullata";
+                MessageBox.Show("Operazione cancellata");
             }
             catch (Exception ex)
             {
+                timer.Stop();
+                dispatcherTimer.Stop();
                 BtnStopScan.IsEnabled = false;
                 BtnStartScan.IsEnabled = true;
                 MessageBox.Show(ex.Message);
@@ -218,31 +197,22 @@ namespace portScanner
 
         private void BtnStopScan_Click(object sender, RoutedEventArgs e)
         {
-            if (mre.WaitOne(0))
+            // ✅ FIX: logica corretta — cancella se NON è già stato cancellato
+            if (!Source.IsCancellationRequested)
             {
-                mre.Reset();
-                BtnStopScan.Content = "▶ RIPRENDI";
-                BtnStartScan.Content = "▶ AVVIA NUOVA";
+                Source.Cancel();
                 timer.Stop();
                 dispatcherTimer.Stop();
-                LblScanStatus.Text = "In attesa...";
+                LblScanStatus.Text = "Scansione fermata";
+                BtnStopScan.IsEnabled = false;
                 BtnStartScan.IsEnabled = true;
-            }
-            else
-            {
-                timer.Start();
-                dispatcherTimer.Start();
-                LblScanStatus.Text = "Scansione in corso...";
-                mre.Set();
-                BtnStopScan.Content = "⏹ FERMA";
-                BtnStartScan.Content = "▶ AVVIA";
-                BtnStartScan.IsEnabled = false;
             }
         }
 
         private void SldThread_Change(object sender, RoutedEventArgs e)
         {
-            LblThreadValue.Content = "Thread: " + SldThread.Value;
+            if (LblThreadValue != null)
+                LblThreadValue.Content = "Thread: " + SldThread.Value;
         }
 
         private void BtnExportCSV_Click(object sender, RoutedEventArgs e)
@@ -253,13 +223,13 @@ namespace portScanner
                 sfd.Filter = "File CSV(*.csv)|*.csv";
                 if (sfd.ShowDialog() == true)
                     using (FileStream fs = new FileStream(sfd.FileName, FileMode.OpenOrCreate, FileAccess.Write))
-                        using (StreamWriter sw = new StreamWriter(fs))
-                            foreach (Scansione line in Scansioni)
-                                sw.WriteLine(line);
+                    using (StreamWriter sw = new StreamWriter(fs))
+                        foreach (Scansione line in Scansioni)
+                            sw.WriteLine(line);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Export non riuscito" + ex.Message);
+                MessageBox.Show("Export non riuscito: " + ex.Message);
             }
         }
     }
--- portScanner/Models/Scansione.cs
+++ portScanner/Models/Scansione.cs
@@ -1,59 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Windows.Media;
 
 namespace portScanner.Models
 {
-    /// <summary>
-    /// Protocollo di rete usato per la scansione.
-    /// È un [Flags] enum, quindi si possono combinare più valori:
-    /// es. Protocollo.TCP | Protocollo.UDP
-    /// </summary>
     [Flags]
     public enum Protocollo
     {
-        Unknow = 0, // nessun protocollo impostato
-        TCP = 1,    // Transmission Control Protocol (orientato alla connessione)
-        UDP = 2     // User Datagram Protocol (senza connessione)
+        Unknow = 0,
+        TCP = 1,
+        UDP = 2
     }
 
-    /// <summary>
-    /// Stato rilevato di una porta durante la scansione.
-    /// </summary>
     public enum StatoPorta
     {
-        Unknown = 0, // stato non ancora determinato
-        Aperta,      // c'è un servizio in ascolto
-        Chiusa,      // nessun servizio in ascolto, l'host risponde con rifiuto
-        Filtrata     // un firewall blocca la risposta
+        Unknown = 0,
+        Aperta,
+        Chiusa,
+        Filtrata
     }
 
-    /// <summary>
-    /// Rappresenta il risultato della scansione di una singola porta.
-    /// </summary>
     public class Scansione
     {
-        // ───────────────────────────────────────────
-        // CAMPI PRIVATI
-        // ───────────────────────────────────────────
-
         private int _porta;
         private int _latenza;
 
-        // ───────────────────────────────────────────
-        // PROPRIETÀ
-        // ───────────────────────────────────────────
-        /// <summary>
-        /// Indirizzo IP o Hostname della scansione
-        /// </summary>
         public string Indirizzo_Hostname { get; set; }
-        /// <summary>
-        /// Numero della porta (1-65535).
-        /// Lancia un'eccezione se il valore è fuori range.
-        /// </summary>
+
         public int Porta
         {
             get => _porta;
@@ -62,43 +37,13 @@ namespace portScanner.Models
                 : value;
         }
 
-        /// <summary>
-        /// Protocollo usato per la scansione (TCP, UDP o entrambi).
-        /// </summary>
         public Protocollo Protocollo { get; set; } = Protocollo.Unknow;
-
-        /// <summary>
-        /// Stato della porta rilevato durante la scansione.
-        /// </summary>
         public StatoPorta Stato { get; set; } = StatoPorta.Unknown;
-
-        /// <summary>
-        /// Nome del servizio in ascolto sulla porta (es. "Apache", "OpenSSH").
-        /// Ottenuto tramite banner grabbing o dizionario porte note.
-        /// </summary>
         public string Servizio { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Versione del servizio in ascolto (es. "2.4.51", "8.9").
-        /// Ottenuta leggendo il banner del servizio.
-        /// </summary>
         public string VersioneServizio { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Data e ora in cui è stata eseguita la scansione di questa porta.
-        /// </summary>
         public DateTime Data { get; set; } = DateTime.Now;
-
-        /// <summary>
-        /// Messaggio di benvenuto inviato automaticamente dal servizio
-        /// quando ci si connette alla porta (es. "SSH-2.0-OpenSSH_8.9").
-        /// </summary>
         public string Banner { get; set; } = string.Empty;
 
-        /// <summary>
-        /// Tempo di risposta della porta in millisecondi.
-        /// Lancia un'eccezione se il valore è negativo.
-        /// </summary>
         public int Latenza
         {
             get => _latenza;
@@ -107,85 +52,33 @@ namespace portScanner.Models
                 : value;
         }
 
-        /// <summary>
-        /// Note aggiuntive sulla porta (generate automaticamente o scritte dall'utente).
-        /// </summary>
         public string Note { get; set; } = string.Empty;
 
-        public string StatoColore
+        public string StatoColore => Stato switch
         {
-            get
-            {
-                switch (Stato)
-                {
-                    case StatoPorta.Aperta:
-                        return "#10B981";
-                    case StatoPorta.Chiusa:
-                        return "#EF4444";
-                    case StatoPorta.Filtrata:
-                        return "#F59E0B";
-                    default:
-                        return "#4B5563";
-                }
-            }
-        }
-        public string StatoIcona
-        {
-            get
-            {
-                switch (Stato)
-                {
-                    case StatoPorta.Aperta:
-                        return "pack://application:,,,/img/open.png";
-                    case StatoPorta.Chiusa:
-                        return "pack://application:,,,/img/closed.png";
-                    case StatoPorta.Filtrata:
-                        return "pack://application:,,,/img/filtered.png";
-                    default:
-                        return "pack://application:,,,/img/unknow.png";
-                }
-            }
-        }
-
-        // ───────────────────────────────────────────
-        // PORTE CRITICHE
-        // ───────────────────────────────────────────
-
-        /// <summary>
-        /// Lista di porte considerate critiche dal punto di vista della sicurezza.
-        /// static = condivisa tra tutte le istanze, readonly = non modificabile.
-        /// </summary>
-        private static readonly List<int> _porteCritiche = new()
-        {
-            21,   // FTP  - trasferisce credenziali in chiaro
-            23,   // Telnet - protocollo non sicuro
-            445,  // SMB - condivisione file Windows, spesso vulnerabile
-            512,  // rexec - esecuzione remota non sicura
-            513,  // rlogin - login remoto non sicuro
-            514,  // rsh - shell remota non sicura
-            1433, // SQL Server - database esposto
-            3389  // RDP - desktop remoto Windows, bersaglio di brute force
+            StatoPorta.Aperta => "#10B981",
+            StatoPorta.Chiusa => "#EF4444",
+            StatoPorta.Filtrata => "#F59E0B",
+            _ => "#4B5563"
         };
 
-        /// <summary>
-        /// Restituisce true se la porta è aperta ED è nella lista delle porte critiche.
-        /// Proprietà calcolata automaticamente, non ha bisogno di essere impostata.
-        /// </summary>
+        public string StatoIcona => Stato switch
+        {
+            StatoPorta.Aperta => "pack://application:,,,/img/open.png",
+            StatoPorta.Chiusa => "pack://application:,,,/img/closed.png",
+            StatoPorta.Filtrata => "pack://application:,,,/img/filtered.png",
+            _ => "pack://application:,,,/img/unknow.png"
+        };
+
+        private static readonly List<int> _porteCritiche = new()
+        {
+            21, 23, 445, 512, 513, 514, 1433, 3389
+        };
+
         public bool IsCritical => _porteCritiche.Contains(Porta) && Stato == StatoPorta.Aperta;
 
-        // ───────────────────────────────────────────
-        // COSTRUTTORI
-        // ───────────────────────────────────────────
-
-        /// <summary>
-        /// Costruttore vuoto. Tutte le proprietà assumono i valori di default.
-        /// </summary>
         public Scansione() { }
 
-        /// <summary>
-        /// Costruttore parametrizzato. Solo la porta è obbligatoria,
-        /// tutti gli altri parametri sono opzionali.
-        /// </summary>
         public Scansione(string indirizzo_Hostname, int porta,
                          Protocollo protocollo = Protocollo.Unknow,
                          StatoPorta stato = StatoPorta.Unknown,
@@ -206,68 +99,64 @@ namespace portScanner.Models
             Note = note;
         }
 
-        // ───────────────────────────────────────────
-        // METODI
-        // ───────────────────────────────────────────
-
-        /// <summary>
-        /// Restituisce una stringa con tutti i dati della scansione separati da virgola.
-        /// Utile per il log e l'export CSV.
-        /// </summary>
         public override string ToString()
         {
             return $"{Indirizzo_Hostname},{Porta},{Protocollo},{Stato},{Servizio},{VersioneServizio},{Data},{Banner},{Latenza},{Note}";
         }
 
-        /// <summary>
-        /// Due scansioni sono uguali se hanno la stessa porta e lo stesso protocollo.
-        /// (TCP porta 80 e UDP porta 80 sono considerate diverse)
-        /// </summary>
         public override bool Equals(object? obj)
         {
             if (obj is not Scansione other) return false;
-            return Indirizzo_Hostname == other.Indirizzo_Hostname && Porta == other.Porta && Protocollo == other.Protocollo;
+            return Indirizzo_Hostname == other.Indirizzo_Hostname
+                && Porta == other.Porta
+                && Protocollo == other.Protocollo;
         }
 
-        /// <summary>
-        /// Operatore == con gestione del null su entrambi i lati.
-        /// </summary>
         public static bool operator ==(Scansione? a, Scansione? b)
         {
             if (a is null && b is null) return true;
             if (a is null || b is null) return false;
             return a.Equals(b);
         }
-        /// <summary>
-        /// Operatore != opposto di ==.
-        /// </summary>
+
         public static bool operator !=(Scansione? a, Scansione? b) => !(a == b);
 
-        /// <summary>
-        /// Necessario quando si sovrascrive Equals.
-        /// Combina porta e protocollo per generare un codice univoco.
-        /// </summary>
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Indirizzo_Hostname, Porta, Protocollo);
-        }
+        public override int GetHashCode() =>
+            HashCode.Combine(Indirizzo_Hostname, Porta, Protocollo);
 
-        public async Task ScanTCP()
+        public async Task ScanTCPAsync(CancellationToken externalToken = default)
         {
             Protocollo = Protocollo.TCP;
             try
             {
-                using (TcpClient tcpClient = new())
+                using var timeoutCts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+                using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(
+                    externalToken,
+                    timeoutCts.Token
+                );
+
+                using TcpClient tcpClient = new();
+
+                await tcpClient.ConnectAsync(Indirizzo_Hostname, Porta, linkedCts.Token);
+
+                Stato = StatoPorta.Aperta;
+                try
                 {
-                    using (CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromSeconds(3)))
-                    {
-                        await tcpClient.ConnectAsync(Indirizzo_Hostname, Porta, cts.Token);
-                        Stato = StatoPorta.Aperta;
-                    }
+                    tcpClient.ReceiveTimeout = 1000;
+                    NetworkStream stream = tcpClient.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length,
+                        new CancellationTokenSource(1000).Token);
+                    if (bytesRead > 0)
+                        Banner = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim();
                 }
+                catch { /* banner grabbing fallito, non è un errore critico */ }
             }
             catch (OperationCanceledException)
             {
+                if (externalToken.IsCancellationRequested)
+                    throw;
+
                 Stato = StatoPorta.Filtrata;
             }
             catch (SocketException)
@@ -279,12 +168,13 @@ namespace portScanner.Models
                 Stato = StatoPorta.Unknown;
             }
         }
-        public static async Task<Scansione> ScanTCP(string indirizzo, int porta)
+
+        public static async Task<Scansione> ScanTCPAsync(string indirizzo, int porta,
+            CancellationToken token = default)
         {
             Scansione s = new(indirizzo, porta);
-            await s.ScanTCP();
+            await s.ScanTCPAsync(token);
             return s;
         }
     }
-
 }
